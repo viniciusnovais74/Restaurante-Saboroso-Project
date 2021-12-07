820ceaf0c0fe5d5b4194fc4c381b15b3d91fd60f
viniciusnovais74 <viniciusnovais74@gmail.com>
2021-12-07 17:42:15 -0300

    Concluindo os Includes de redirecionameto

--- routes/index.js
+++ routes/index.js
@@ -25,28 +25,36 @@ router.get('/', function (req, res, next) {
 router.get('/contacts', function(req,res,next){
 
   res.render('contact',{
-    title:'Contato - Restaurante Saboroso'
+    title:'Contato - Restaurante Saboroso',
+    background: 'images/img_bg_3.jpg',
+    h1: 'Diga um oi!'
   });
 
 })
 router.get('/menus', function(req,res,next){
 
   res.render('menu',{
-    title:'Menus - Restaurante Saboroso'
+    title:'Menus - Restaurante Saboroso',
+    background: 'images/img_bg_1.jpg',
+    h1: 'Saboreie nosso menu!'
   });
   
 })
 router.get('/reservations', function(req,res,next){
 
   res.render('reservation',{
-    title:'Reservas  - Restaurante Saboroso'
+    title:'Reservas  - Restaurante Saboroso',
+    background: 'images/img_bg_2.jpg',
+    h1: 'Reserve uma Mesa!'
   });
   
 })
 router.get('/services', function(req,res,next){
 
   res.render('services',{
-    title:'Serviços - Restaurante Saboroso'
+    title:'Serviços - Restaurante Saboroso',
+    background: 'images/img_bg_1.jpg',
+    h1: 'É um prazer poder servir!'
   });
   
 })
