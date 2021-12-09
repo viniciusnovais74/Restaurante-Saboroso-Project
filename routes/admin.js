be901a6606dbfd04b67716f60c5ad0fc00b2dc39
Vinicius Novais <viniciusnovais74@gmail.com>
2021-12-08 22:17:54 -0300

    Atualizando Rotas Administrativas

--- routes/admin.js
+++ routes/admin.js
@@ -3,5 +3,59 @@ var router = express.Router();
 
 router.get('/', function(req,res,next){
 
-    res.render("admin/index")
-}) 
+    res.render("admin/index", {
+
+    });
+});
+
+router.get('/login', function(req,res,next){
+
+if (!req.session.views) req.session.views = 0;
+
+    res.render("admin/login", {
+
+    });
+});
+
+router.get('/login', function(req,res,next){
+
+    res.render("admin/contacts", {
+
+    });
+});
+
+router.get('/contacts', function(req,res,next){
+
+    res.render("admin/contacts", {
+
+    });
+});
+
+router.get('/emails', function(req,res,next){
+
+    res.render("admin/emails", {
+
+    });
+});
+
+router.get('/menus', function(req,res,next){
+
+    res.render("admin/menus", {
+
+    });
+});
+
+router.get('/reservations', function(req,res,next){
+
+    res.render("admin/reservations", {
+
+    });
+});
+
+router.get('/users', function(req,res,next){
+
+    res.render("admin/users", {
+
+    });
+});
+module.exports = router;
